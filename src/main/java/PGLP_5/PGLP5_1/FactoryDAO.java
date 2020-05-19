157d4ad20c6f018ec56d049aa33495174c222c70
KI Serge Laobien <sergelaobienk@gmail.com>
2020-05-19 21:53:18 +0000

    mis a jour de la classeFactoryDAOtest

--- src/main/java/PGLP_5/PGLP5_1/FactoryDAO.java
+++ src/main/java/PGLP_5/PGLP5_1/FactoryDAO.java
@@ -4,22 +4,15 @@ import java.sql.Connection;
 
 public class FactoryDAO{
 	
-	public static DAO<Personnels> PersonnelsDAO(Connection conn){
+private static final Connection conn=EntrepriseConn.EntrepriseConn();
+	
+	
+	public static PersonnelsDAO getPersonnelsDAO(){
 		return new PersonnelsDAO(conn);
 	}
 	
-	public static DAO<CompositePersonnels> getCompositePersonnels(Connection conn){
+	public static CompositePersonnelsDAO getCompositePerso(){
 		return new CompositePersonnelsDAO(conn);
-	}
-
-	public static PGLP_5.PGLP5_1.PersonnelsDAO getPersonnelsDAO() {
-		// TODO Auto-generated method stub
-		return null;
-	}
-
-	public static PGLP_5.PGLP5_1.PersonnelsDAO getPersonnelsDao() {
-		// TODO Auto-generated method stub
-		return null;
 	}
 
 
